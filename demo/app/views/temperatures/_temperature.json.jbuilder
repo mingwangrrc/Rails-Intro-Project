91bdff9bba76913c3e5a7c5f5a198af684829584
mingwang <mingwang@academic.rrc.ca>
2024-10-22 21:19:49 +0800

    add Collection Navigation

---
+++ demo/app/views/temperatures/_temperature.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! temperature, :id, :avg_temp, :date, :city_id, :created_at, :updated_at
+json.url temperature_url(temperature, format: :json)
--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :temperatures
+  resources :cities
   get 'about/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
